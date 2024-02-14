c39b88a867d316390fb02ccb69c5ff58ac5e7225
Md Asaduzzaman Rana <iamtheasad@gmail.com>
2024-02-14 08:24:33 +0600

    :gem: feat: events and props drilling

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
+import React from "react";
 
-function FeedbackList({ feedback }) {
+import FeedbackItem from "./FeedbackItem";
+
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return "No Feedback yet";
   }
@@ -10,7 +11,7 @@ function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
