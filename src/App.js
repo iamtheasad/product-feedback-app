40c0489d19a50f879227f490af285fb08f859ee6
Md Asaduzzaman Rana <iamtheasad@gmail.com>
2024-02-28 08:28:17 +0600

    :gem: feat: dynamic feedback item added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    console.log(newFeedback);
+    setFeedback([newFeedback, ...feedback]);
   };
 
   const deleteFeedback = (id) => {
